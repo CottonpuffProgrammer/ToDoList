dc3896621719e5466549742807c8e6d3258de706
CottonpuffProgrammer <105884584+CottonpuffProgrammer@users.noreply.github.com>
2022-05-19 15:24:27 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,6 @@ class ToDoTask {
     task:string;
     date:string;
     completed:boolean;
-
-    constructor(inputTask:string) {
-        this.task = inputTask;
-    }
 }
 
 /* let task = new ToDoTask("Test");
@@ -18,36 +14,66 @@ function isValid():boolean {
     return true;
 }
 
-function displayTask () {
+function displayTask (task:ToDoTask) {
+    let taskText = document.createElement("h2");
+    taskText.innerText = task.task.toString();
 
+    let taskDate = document.createElement("p");
+    taskDate.innerText = task.date.toString();
+
+    let taskDiv = document.createElement("div");
+    if(task.completed) { /* adds completed task 
+                            to class for styling */
+        taskDiv.classList.add("complete");
+    }
+
+    taskDiv.appendChild(taskText);
+    taskDiv.appendChild(taskDate);
+
+    if(task.completed) {
+        let completedTasks = document.getElementById("complete");
+        completedTasks.appendChild(taskDiv);
+    }
+    else {
+        let notCompletedTasks = document.getElementById("incomplete");
+        notCompletedTasks.appendChild(taskDiv);
+    }
 }
 
 function getElement(id):HTMLInputElement {
+    // returns an element as a HTMLInputElement
     return <HTMLInputElement>document.getElementById(id);
 }
 
 function getTask():ToDoTask {
     let thisTask = new ToDoTask();
 
+    // get name of task
     let taskInput = getElement("task");
     thisTask.task = taskInput.value;
 
+    // get date of task
     let dateInput = getElement("date");
     thisTask.date = dateInput.value;
 
+    // get completion statuc
     let completed = getElement("completed");
     thisTask.completed = completed.checked;
 
+    // return the task object
+    return thisTask;
 }
 
 function main() {
-    if(isValid()) {
-        let task = getTask();
-        displayTask(task);
+    if(isValid()) { // if valid
+        let task = getTask(); // get task information
+        displayTask(task); // display the task information
     }
 }
 
 window.onload = function() {
-    let taskToAdd = document.getElementById();
-    addTask.onclick = main;
+    // get button from HTML
+    let taskToAdd = document.getElementById("addTask");
+    // executes main on button click
+    taskToAdd.onclick = main;
 }
